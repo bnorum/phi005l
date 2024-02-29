8f43eb4bf4955f8e686a2b6767256d61280a502c
brady norum <115905633+bnorum@users.noreply.github.com>
2024-02-29 12:05:08 -0500

    Update DecisionLine.cs

--- Assets/Scripts/DecisionLine.cs
+++ Assets/Scripts/DecisionLine.cs
@@ -4,5 +4,35 @@ using UnityEngine;
 
 public class DecisionLine : ScriptLine
 {
-    
+     private string content;
+    public string Content {
+        get { return content; }
+        set { content = value; }
+    }
+    private float delay;
+    public float Delay {
+        get { return delay; }
+        set { delay = value; }
+    }
+    private string speaker;
+    public string Speaker {
+        get { return speaker; }
+        set { speaker = value; }
+    }   void ConvertName() {
+        if (speaker == "b") {
+            speaker = "Boss";
+        }
+        if (speaker == "c") {
+            speaker = "Chuckles";
+        }
+    }
+    public DecisionLine(string Speaker = "Narrator", float Delay = 1,string Content = "This is a test.")
+    {
+        this.content = Content;
+        this.speaker = Speaker;
+        this.delay = Delay;
+        ConvertName();
+    }
+
+
 }
